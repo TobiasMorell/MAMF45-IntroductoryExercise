a8f7cb1e2a16407a06628e97416a9238d7a0456f
Tobias Morell <tmorel14@student.aau.dk>
2018-03-06 13:40:48 +0100

    Added lamps - still problems with small interactable items

---
+++ Assets/Scripts/Lamp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts;
+using UnityEngine;
+
+public class Lamp : MonoBehaviour, IInteractable
+{
+    public Light Light;
+
+    public string Description
+    {
+        get { return "Toggle light"; }
+    }
+
+    public void Interact()
+    {
+        Light.enabled = !Light.enabled;
+    }
+}
--- Assets/Scripts/Laptop.cs
+++ Assets/Scripts/Laptop.cs
@@ -18,22 +18,26 @@ public class Laptop : MonoBehaviour, IInteractable
         _screenOpen = Screen.localRotation;
         _screenClosed = Quaternion.Euler(90f, 0f, 0f);
     }
-	
-	// Update is called once per frame
-	void Update () {
-	    if (_startTime > 0)
-	    {
-	        var step = (Time.time - _startTime) * Speed;
-	        Screen.localRotation = _open
-	            ? Quaternion.Lerp(_screenClosed, _screenOpen, step)
-	            : Quaternion.Lerp(_screenOpen, _screenClosed, step);
-	    }
-    }
 
     public string Description { get; private set; }
     public void Interact()
     {
-        _open = !_open;
         _startTime = Time.time;
+        StartCoroutine(ChangeLidRotation(_open ? _screenOpen : _screenClosed, _open ? _screenClosed : _screenOpen));
+    }
+
+    private IEnumerator ChangeLidRotation(Quaternion start, Quaternion end)
+    {
+        while (_startTime > 0)
+        {
+            var step = (Time.time - _startTime) * Speed;
+            Screen.localRotation = Quaternion.Lerp(start, end, step);
+            yield return null;
+        }
+
+        _open = !_open;
+        var col = GetComponent<BoxCollider>();
+        col.center = new Vector3(0, _open ? 0.1f : 0.01f);
+        col.size = new Vector3(0.3f, _open ? 0.2f : 0.02f, 0.2f);
     }
 }
